2c9b0384f1a48845d4ef1fc13dce1d6735c398e7
melnichenko1996 <djek678@gmail.com>
2018-12-11 07:13:56 +0200

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users, path: 'users', controllers: { 
+    sessions: "users/sessions",
+    registrations: "users/registrations",
+    passwords: "users/passwords",
+    confirmations: "users/confirmations"
+  }
+  
+  devise_for :admins, path: 'admins', controllers: { 
+    sessions: "admins/sessions",
+    registrations: "admins/registrations", 
+  }
+  
+  get 'home/index'
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
